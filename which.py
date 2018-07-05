c0a441fc392be4cd70359d800e7895cf880de521
Spencer Franks <spencer.franks@thetileapp.com>
2018-07-05 14:48:00 -0700

    Update which.py

--- which.py
+++ which.py
@@ -4,18 +4,29 @@ highest = 10
 answer = random.randint(1, highest)
 
 print("Please guess a number between 1 and {}: ".format(highest))
-guess = int(input())
+# guess = int(input())
+# while guess != answer:
+#     if guess == 0:
+#         print("Game over")
+#         break
+#     if guess < answer:
+#         print("Try higher")
+#         guess = int(input())
+#     if guess > answer:
+#         print("Try lower")
+#         guess = int(input())
+#     continue
+#
+# else:
+#     print("Congratulations!")
+guess = 0
 while guess != answer:
+    guess = int(input())
     if guess == 0:
-        print("Game over")
         break
     if guess < answer:
         print("Try higher")
-        guess = int(input())
-    if guess > answer:
+    elif guess > answer:
         print("Try lower")
-        guess = int(input())
-    continue
-
-else:
-    print("Congratulations!")
+    else:
+        print("Congratulations!")
